5aa19a1d382c73aab46165faf1e9034b8b6abf9d
vishaljadhav82 <vishalvp8261@gmail.com>
2024-10-09 12:32:45 +0530

    remove vercel analytice from package.lock.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['firebasestorage.googleapis.com','placehold.co'], // Add your Firebase Storage domain here
+        domains: ['firebasestorage.googleapis.com'], // Add your Firebase Storage domain here
       },
 };
 
