4ea4d6843a8113baf18b58140ac463bb1014f41f
vishaljadhav82 <vishalvp8261@gmail.com>
2024-10-09 12:25:46 +0530

    js

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,6 +30,15 @@ export const metadata = {
   }
 };
 
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = false
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
+export const maxDuration = 5
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
